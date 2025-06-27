949f40f167d36847c19b364325e3601cd2178d2f
Maikel Koek <maikel@h-o.nl>
2025-06-27 14:22:47 +0200

    feat: possibility to export stock xml via config

---
+++ Block/Adminhtml/System/Config/StockExportButton.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Copyright © Reach Digital (https://www.reachdigital.io/)
+ * See LICENSE.txt for license details.
+ */
+
+declare(strict_types=1);
+
+namespace ReachDigital\Vendit\Block\Adminhtml\System\Config;
+
+use Magento\Config\Block\System\Config\Form\Field;
+use Magento\Framework\Data\Form\Element\AbstractElement;
+use Magento\Backend\Block\Template\Context;
+use Magento\Framework\UrlInterface;
+
+class StockExportButton extends Field
+{
+    protected $_template = 'ReachDigital_Vendit::system/config/export_button.phtml';
+
+    public function __construct(Context $context, public UrlInterface $urlBuilder, array $data = [])
+    {
+        parent::__construct($context, $data);
+    }
+
+    protected function _getElementHtml(AbstractElement $element): string
+    {
+        return $this->_toHtml();
+    }
+
+    public function getExportUrl(): string
+    {
+        return $this->urlBuilder->getUrl('vendit/stockExport/run');
+    }
+}
